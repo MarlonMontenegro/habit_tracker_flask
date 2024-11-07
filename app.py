dc2267544bdf49bcb1b72990f38a92c5d1ab8066
Marlon Montenegro <marlonmontenegropaz@gmail.com>
2024-11-07 14:56:40 -0600

    Add configuration and MongoDB

--- app.py
+++ app.py
@@ -1,56 +1,16 @@
-from datetime import datetime, timedelta
-from collections import defaultdict
-from flask import Flask, render_template, request, redirect, url_for
-import datetime
+import os
+from flask import Flask
+from routes import pages
+from pymongo import MongoClient
+from dotenv import load_dotenv
 
-app = Flask(__name__)
-habits = ["test habit"]
-completions = defaultdict(list)
-
-
-@app.context_processor
-def add_calc_date_range():
-    def date_range(start: datetime.date):
-        dates = [start + timedelta(days=diff) for diff in range(-3, 4)]
-        return dates
-
-    return {"dates_range": date_range}
+load_dotenv()
 
 
-@app.route("/")
-def index():
-    date_str = request.args.get("date")
-    if date_str:
-        selected_date = datetime.date.fromisoformat(date_str)
-    else:
-        selected_date = datetime.date.today()
+def create_app():
+    app = Flask(__name__)
+    client = MongoClient(os.environ.get("MONGODB_URI"))
+    app.db = client["tracker"]
 
-    return render_template("index.html",
-                           habits=habits,
-                           title="Habit Tracker - Home",
-                           selected_date=selected_date,
-                           completions=completions[selected_date]
-                           )
-
-
-@app.route("/add", methods=["GET", "POST"])
-def add_habit():
-    if request.method == "POST":
-        habit = request.form.get("habit")
-        habits.append(habit)
-
-    return render_template("add_habit.html", title="Habit Tracker - Add Habit", selected_date=datetime.date.today())
-
-
-@app.route("/complete", methods=["POST"])
-def complete():
-    date_string = request.form.get("date")
-    habit = request.form.get("habitName")
-    date = datetime.date.fromisoformat(date_string)
-    completions[date].append(habit)
-
-    return redirect(url_for("index", date=date_string))
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
+    app.register_blueprint(pages)
+    return app
